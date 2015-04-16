aa231bb30e0da145894870e87f35d1ebce9813b3
Rubi1 <sarkar86rubi@gmail.com>
2015-04-16 14:14:24 +0530

    lil bit changes

--- ManagementSystem/src/main/java/com/infotop/management/subject/web/SubjectController.java
+++ ManagementSystem/src/main/java/com/infotop/management/subject/web/SubjectController.java
@@ -293,13 +293,7 @@ public class SubjectController extends BasicController {
 			if (user != null) {
 				
 			 	Subject entity = subjectService.get(id); 
-			 	 List<Department> deptList = deptService.getAllDepts();
-				 List<Batch> batchList = batchService.getAllBatches();
-				 
-				 model.addAttribute("depts", deptList);
-				 model.addAttribute("sem", batchList);
-		        
-			 	
+			 
 			 	model.addAttribute("subject", entity);
 			} else {
 				logger.log(this.getClass(),Logger.ERROR_INT,"登陆帐号无效!","",null);
